ce64b491dae0dd641461c552b54ebafc58fb6ccf
wangjianlong <52690386+Wang-JianLong@users.noreply.github.com>
2023-02-15 21:44:35 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This My Website</title>
+</head>
+<body>
+
+    <script src="./js/index.js"> </script>
+
+   <per><h1>Welcome to visit my webpage, here is under construction ...</h1> </per>
+</body>
+</html>
